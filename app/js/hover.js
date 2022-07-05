0e190c574b5f97176f1c578fa4472e0f5df5bef3
Nicholas Panayotakos <nicholaspanayotakos@gmail.com>
2022-07-05 16:51:35 -0400

    added faq page

--- app/js/hover.js
+++ app/js/hover.js
@@ -1,23 +1,36 @@
 function plugIn() {
-  document.querySelector(".income__plug").style.transform = "translate(-13px)"
-  document.querySelector(".income__passive").style.color = "var(--primary-color)"
+  document.querySelector(".income__plug").style.transform = "translate(-13px)";
+  document.querySelector(".income__passive").style.color = "var(--primary-color)";
 }
 
 function plugOut() {
-  document.querySelector(".income__plug").style.transform = "translate(0)"
-  document.querySelector(".income__passive").style.color = "var(--blue-900)"
+  document.querySelector(".income__plug").style.transform = "translate(0)";
+  document.querySelector(".income__passive").style.color = "var(--blue-900)";
 }
 
 function showChatBubble() {
-  document.querySelector(".testimonials__left-person").style.transform = "translateX(0px)"
-  document.querySelector(".testimonials__right-person").style.transform = "translateX(0px)"
-  document.querySelector(".testimonials__chat-bubble").style.transform = "scale(1) rotate(0deg)"
-  document.querySelector(".testimonials__chat-bubble").style.opacity = "1"
+  document.querySelector(".testimonials__left-person").style.transform = "translateX(0px)";
+  document.querySelector(".testimonials__right-person").style.transform = "translateX(0px)";
+  document.querySelector(".testimonials__chat-bubble").style.transform = "scale(1) rotate(0deg)";
+  document.querySelector(".testimonials__chat-bubble").style.opacity = "1";
 }
 
 function hideChatBubble() {
-  document.querySelector(".testimonials__left-person").style.transform = "translateX(-10px)"
-  document.querySelector(".testimonials__right-person").style.transform = "translateX(10px)"
-  document.querySelector(".testimonials__chat-bubble").style.transform = "scale(0) rotate(-90deg)"
-  document.querySelector(".testimonials__chat-bubble").style.opacity = "0"
+  document.querySelector(".testimonials__left-person").style.transform = "translateX(-10px)";
+  document.querySelector(".testimonials__right-person").style.transform = "translateX(10px)";
+  document.querySelector(".testimonials__chat-bubble").style.transform = "scale(0) rotate(-90deg)";
+  document.querySelector(".testimonials__chat-bubble").style.opacity = "0";
 }
+
+const questionList = document.querySelectorAll(".faq__question");
+
+// questionList.forEach((question) => {
+//   question.addEventListener("click", function () {
+//     console.log("test")
+//     question.currentTarget.classList.toggle("test");
+//   });
+// });
+
+questionList.forEach(question => question.addEventListener("click", () => {
+  question.classList.toggle("toggle");
+}))
