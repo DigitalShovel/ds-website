bad9b2b3fa9800182b26a3a39f26d4977615dd54
Nicholas Panayotakos <nicholaspanayotakos@gmail.com>
2022-08-10 16:09:32 -0400

    form fix 3

--- app/js/contact.js
+++ app/js/contact.js
@@ -1,9 +1,12 @@
-const orderBtn = document.querySelector("#order");
+const orderBtn = document.querySelector("#Amount-of-Minipods");
 const order = document.querySelector(".fieldset__order");
-const messageBtn = document.querySelector("#message");
+const messageBtn = document.querySelector("#Message");
 const message = document.querySelector(".fieldset__message");
-const bothBtn = document.querySelector("#both");
+const bothBtn = document.querySelector("#Message-and-Order");
 const both = document.querySelector(".fieldset__order .fieldset__message");
+
+const radioSelection = document.querySelector("input");
+const radioLabel = document.querySelector(".fieldset__radio");
 
 orderBtn.addEventListener("click", () => {
   order.style.display = "flex";
@@ -19,3 +22,10 @@ bothBtn.addEventListener("click", () => {
   message.style.display = "flex";
   order.style.display = "flex";
 });
+
+
+if (radioSelection.hasAttribute("checked")) {
+  radioLabel.classList.add("input[type=radio]:checked+label");
+} else {
+  radioLabel.classList.remove("input[type=radio]:checked+label");
+}
