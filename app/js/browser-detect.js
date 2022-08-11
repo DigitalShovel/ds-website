17c2bb79688a77297840424e9920ae977e68a0ed
Nicholas Panayotakos <nicholaspanayotakos@gmail.com>
2022-08-11 12:17:39 -0400

    safari dropdown fix #2

--- app/js/browser-detect.js
+++ app/js/browser-detect.js
@@ -2,20 +2,9 @@ function fnBrowserDetect() {
   let userAgent = navigator.userAgent;
   let fan = document.querySelectorAll(".fan-swing-base");
 
-  if (userAgent.match(/chrome|chromium|crios/i)) {
-    browserName = "chrome";
-  } else if (userAgent.match(/firefox|fxios/i)) {
-    browserName = "firefox";
-  } else if (userAgent.match(/safari/i)) {
-    console.log("it worked for safari");
+  if (userAgent.match(/safari/i)) {
     for (let i = 0; i < fan.length; i++) {
       fan[i].classList.add("only-safari");
     }
-  } else if (userAgent.match(/opr\//i)) {
-    browserName = "opera";
-  } else if (userAgent.match(/edg/i)) {
-    browserName = "edge";
-  } else {
-    browserName = "No browser detection";
   }
 }
