c1e03be8c0d405fc952ea1f191b85d5b37f86502
Nicholas Panayotakos <nicholaspanayotakos@gmail.com>
2022-08-11 11:45:33 -0400

    safari fix #7

--- app/js/browser-detect.js
+++ app/js/browser-detect.js
@@ -1,21 +1,18 @@
-function fnBrowserDetect(){
-                 
+function fnBrowserDetect() {
   let userAgent = navigator.userAgent;
-  let browserName;
-  
-  if(userAgent.match(/chrome|chromium|crios/i)){
-      browserName = "chrome";
-    }else if(userAgent.match(/firefox|fxios/i)){
-      browserName = "firefox";
-    }  else if(userAgent.match(/safari/i)){
-      browserName = "safari";
-    }else if(userAgent.match(/opr\//i)){
-      browserName = "opera";
-    } else if(userAgent.match(/edg/i)){
-      browserName = "edge";
-    }else{
-      browserName="No browser detection";
-    }
-  
-   document.querySelector("h1").innerText="You are using "+ browserName +" browser";         
+
+  if (userAgent.match(/chrome|chromium|crios/i)) {
+    browserName = "chrome";
+  } else if (userAgent.match(/firefox|fxios/i)) {
+    browserName = "firefox";
+  } else if (userAgent.match(/safari/i)) {
+    document.querySelector(".only-safari").style.animation = "none";
+  } else if (userAgent.match(/opr\//i)) {
+    browserName = "opera";
+  } else if (userAgent.match(/edg/i)) {
+    browserName = "edge";
+  } else {
+    browserName = "No browser detection";
+  }
+
 }
