247d0c39860c4b05cecf144d52db57f130035e4a
Nicholas Panayotakos <nicholaspanayotakos@gmail.com>
2022-08-11 12:08:56 -0400

    safari fix #10

--- app/js/browser-detect.js
+++ app/js/browser-detect.js
@@ -1,13 +1,16 @@
 function fnBrowserDetect() {
   let userAgent = navigator.userAgent;
+  let fan = document.querySelectorAll(".fan-swing-base");
 
   if (userAgent.match(/chrome|chromium|crios/i)) {
     browserName = "chrome";
   } else if (userAgent.match(/firefox|fxios/i)) {
     browserName = "firefox";
   } else if (userAgent.match(/safari/i)) {
-    let fan = document.querySelector(".fan-swing-base");
-    fan.classList.add("only-safari");
+    console.log("it worked for safari");
+    for (let i = 0; i < fan.length; i++) {
+      fan[i].classList.add("only-safari");
+    }
   } else if (userAgent.match(/opr\//i)) {
     browserName = "opera";
   } else if (userAgent.match(/edg/i)) {
