f2bdcc45546f6abcf6d1bcf5c3a97296161d336b
Nicholas Panayotakos <nicholaspanayotakos@gmail.com>
2022-08-12 09:35:00 -0400

    iphone animation fix

--- app/js/browser-detect.js
+++ app/js/browser-detect.js
@@ -1,10 +1,15 @@
 function fnBrowserDetect() {
   let userAgent = navigator.userAgent;
   let fan = document.querySelectorAll(".fan-swing-base");
+  console.log(userAgent);
 
-  if (userAgent.match(/safari/i)) {
+  if (userAgent.match(/iPhone/i)) {
     for (let i = 0; i < fan.length; i++) {
       fan[i].classList.add("only-safari");
+    }
+  } else {
+    for (let i = 0; i < fan.length; i++) {
+      fan[i].classList.remove("only-safari");
     }
   }
 }
