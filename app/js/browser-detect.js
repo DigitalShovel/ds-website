5f22eae6d3772b39087ce38599b1f2f7b0ded971
Nicholas Panayotakos <nicholaspanayotakos@gmail.com>
2022-08-12 10:40:57 -0400

    test

--- app/js/browser-detect.js
+++ app/js/browser-detect.js
@@ -6,10 +6,12 @@ function fnBrowserDetect() {
   if (userAgent.match(/iPhone/i)) {
     for (let i = 0; i < fan.length; i++) {
       fan[i].classList.add("only-safari");
+      console.log("it worked!")
     }
   } else {
     for (let i = 0; i < fan.length; i++) {
       fan[i].classList.remove("only-safari");
+      console.log("it didn't work!")
     }
   }
 }
