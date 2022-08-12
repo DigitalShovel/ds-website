040fc3648ae16b3b7f26ed74b42b851c23578f55
Nicholas Panayotakos <nicholaspanayotakos@gmail.com>
2022-08-12 10:50:54 -0400

    apple fix #3

--- app/js/detect-actions.js
+++ app/js/detect-actions.js
@@ -2,7 +2,7 @@ const ua = detect.parse(navigator.userAgent);
 let fan = document.querySelectorAll(".fan-swing-base");
 console.log(ua.os);
 
-if (ua.os.family.includes("iOS")) {
+if (ua.os.family.includes("Mac")) {
   for (let i = 0; i < fan.length; i++) {
     fan[i].classList.add("only-safari");
     console.log("it worked!");
