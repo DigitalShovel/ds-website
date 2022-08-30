c5d10f76da373b61f74282e4b8ad9d9cd4dfbd76
Nicholas Panayotakos <nicholaspanayotakos@gmail.com>
2022-08-30 10:22:47 -0400

    fix subnav for mobile

--- app/js/nav-toggle.js
+++ app/js/nav-toggle.js
@@ -4,6 +4,9 @@ const navToggle = document.querySelector(".navbar__hamburger");
 const body = document.querySelector("body");
 const html = document.querySelector("html");
 
+let configsClass = document.querySelector(".subnav__link--configs");
+let specsClass = document.querySelector(".subnav__link--specs");
+
 navToggle.addEventListener("click", () => {
   const visibility = navLinks.getAttribute("data-visible");
   const expanded = navToggle.getAttribute("aria-expanded");
@@ -15,10 +18,24 @@ navToggle.addEventListener("click", () => {
   }
 
   if (expanded === "true") {
-    html.classList.remove("no-scroll")
+    html.classList.remove("no-scroll");
     navToggle.setAttribute("aria-expanded", false);
+    navLinks.classList.toggle("show");
   } else {
-    html.classList.add("no-scroll")
+    html.classList.add("no-scroll");
     navToggle.setAttribute("aria-expanded", true);
+    navLinks.classList.toggle("show");
   }
 });
+
+window.matchMedia("(max-width: 450px)").onchange = (e) => {
+  if (e.matches) {
+    specsClass.innerHTML = "Specs";
+    configsClass.innerHTML = "Configs";
+    console.log("This is a narrow screen — less than 450px wide.");
+  } else {
+    specsClass.innerHTML = "Specifications";
+    configsClass.innerHTML = "Configurations";
+    console.log("This is a wide screen — more than 450px wide.");
+  }
+};
