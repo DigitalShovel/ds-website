6a7b23b849300efe7006c830f6a05efc53eace80
Nicholas Panayotakos <nicholaspanayotakos@gmail.com>
2022-12-20 16:42:56 -0500

    added detect location

--- app/js/contact.js
+++ app/js/contact.js
@@ -1,28 +1,5 @@
-const orderBtn = document.querySelector("#Minipod-Order-Quantity");
-const order = document.querySelector(".fieldset__order");
-const messageBtn = document.querySelector("#Message");
-const message = document.querySelector(".fieldset__message");
-const bothBtn = document.querySelector("#Message-and-Order");
-const both = document.querySelector(".fieldset__order .fieldset__message");
-
 const radioSelection = document.querySelector("input");
 const radioLabel = document.querySelector(".fieldset__radio");
-
-orderBtn.addEventListener("click", () => {
-  order.style.display = "flex";
-  message.style.display = "none";
-});
-
-messageBtn.addEventListener("click", () => {
-  message.style.display = "flex";
-  order.style.display = "none";
-});
-
-bothBtn.addEventListener("click", () => {
-  message.style.display = "flex";
-  order.style.display = "flex";
-});
-
 
 if (radioSelection.hasAttribute("checked")) {
   radioLabel.classList.add("input[type=radio]:checked+label");
---
+++ app/js/detect-location.js
@@ -0,0 +1,5 @@
+// Purpose: Detect the user's timezone and set the timezone in the select element with id 'timezone'
+let timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+// set the timezone in the select element with id 'timezone'
+document.getElementById("Timezone").value = timezone;
