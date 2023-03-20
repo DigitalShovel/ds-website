c84a5949d4487efd1651ac2806e0fb783fb215a2
Nicholas Panayotakos <nicholaspanayotakos@gmail.com>
2023-03-20 11:28:32 -0400

    added i300 page

---
+++ app/js/detect-scroll-i300.js
@@ -0,0 +1,56 @@
+// detects the scroll position and adds the active class to the corresponding link
+
+let product = document.querySelector("#product");
+let specs = document.querySelector("#specifications");
+let bundle = document.querySelector("#bundle");
+
+let productLink = document.querySelector(".subnav__link--product");
+let specsLink = document.querySelector(".subnav__link--specs");
+let bundleLink = document.querySelector(".subnav__link--bundle");
+
+let check = false;
+
+window.addEventListener("scroll", () => {
+  if (window.innerWidth >= 992) {
+    if (scrollY <= 400) {
+      specsLink.classList.remove("subnav__link--active");
+      bundleLink.classList.remove("subnav__link--active");
+
+      // set the active class to the product link
+      productLink.classList.add("subnav__link--active");
+    } else if (scrollY <= 1250) {
+      console.log("Scroll position: " + scrollY);
+      productLink.classList.remove("subnav__link--active");
+      bundleLink.classList.remove("subnav__link--active");
+
+      // set the active class to the specs link
+      specsLink.classList.add("subnav__link--active");
+    } else if (scrollY > 1250) {
+      productLink.classList.remove("subnav__link--active");
+      specsLink.classList.remove("subnav__link--active");
+
+      // set the active class to the bundle link
+      bundleLink.classList.add("subnav__link--active");
+    }
+  } else {
+    if (scrollY <= 600) {
+      specsLink.classList.remove("subnav__link--active");
+      bundleLink.classList.remove("subnav__link--active");
+
+      // set the active class to the product link
+      productLink.classList.add("subnav__link--active");
+    } else if (scrollY <= 1700) {
+      productLink.classList.remove("subnav__link--active");
+      bundleLink.classList.remove("subnav__link--active");
+
+      // set the active class to the specs link
+      specsLink.classList.add("subnav__link--active");
+    } else if (scrollY > 1700) {
+      productLink.classList.remove("subnav__link--active");
+      specsLink.classList.remove("subnav__link--active");
+
+      // set the active class to the bundle link
+      bundleLink.classList.add("subnav__link--active");
+    }
+  }
+});
