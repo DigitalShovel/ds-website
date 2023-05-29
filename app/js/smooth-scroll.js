9f4787142acfe69e2cfb0e94b0e0516d5fce5f0e
Nicholas Panayotakos <nicholaspanayotakos@gmail.com>
2023-05-29 15:13:07 -0400

    organizing files

--- app/js/smooth-scroll.js
+++ app/js/smooth-scroll.js
@@ -14,4 +14,5 @@ const scrollerSmoother = ScrollSmoother.create({
   smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
   effects: true, // looks for data-speed and data-lag attributes on elements
   smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+  normalizeScroll: true // force the scroll target to always be a positive number
 });
